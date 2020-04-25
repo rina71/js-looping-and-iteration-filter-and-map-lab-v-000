964d3f6ee95b39c20cb118b26b38e2414da47c8d
Yurina Shin <yurinas@gmail.com>
2020-04-25 03:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, number){
+  let newArr = [];
+  for (const driver of drivers){
+     if (driver.revenue > number){
+       newArr.push(driver);
+     }
+  }
+  return newArr
+}
+
+function driverNamesWithRevenueOver(drivers, number){
+  let newArr = [];
+  for (const driver of drivers){
+    if (driver.revenue > number){
+      newArr.push(driver.name);
+    }
+  }
+  return newArr
+}
+
+function exactMatch(drivers, value){
+  let newArr = [];
+  debugger;
+    if (drivers.filter(item => item === value )){
+      newArr.push(item)
+    }
+      return newArr
+  }
+
+
+
+// function driversWithRevenueOver(drivers,revenue){
+//   const driversWithRevenueOver = filter(drivers, function (revenue) { return driver.revenue > revenue; });
+// }
+// const result = array.filter(item => item.name === criteria);
